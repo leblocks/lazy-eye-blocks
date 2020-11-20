95973d6b4969c0d20e5acf106700b471edc3f51c
Alexander Gurevich <alexander.gurevich@earnix.com>
2020-11-20 22:10:52 +0200

    initial commit

---
+++ src/main.js
@@ -0,0 +1,4 @@
+import './index.html';
+import './main.scss';
+
+window.onload = () => console.log('loaded');
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+
+// plugins
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/main.js',
+    plugins: [
+        new CleanWebpackPlugin(),
+    ],
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'eslint-loader',
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            {
+                test: /\.html$/,
+                exclude: /node_modules/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]', // we have to preseve original file name
+                },
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: { name: '[name].css' }, // copy to dist folder with file-loader
+                    },
+                    { loader: 'sass-loader' }, // transpile with sass first
+                ],
+            },
+        ],
+    },
+};
