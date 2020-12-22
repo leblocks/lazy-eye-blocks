aa33055d4c50b466ac8fdedd1ce3a5549db6f057
Alexander Gurevich <alexander.gurevich@earnix.com>
2020-12-22 16:32:26 +0200

    grid-button refactor

--- src/ui/components/grid-button/index.js
+++ src/ui/components/grid-button/index.js
@@ -2,23 +2,19 @@ import { getState, setState, addStateObserver } from '../../../state';
 import { createMenuItem } from '../../utils';
 
 export default function () {
-    const id = 'gridButton';
-
     const onClick = () => {
         const { gridEnabled } = getState();
         setState({ gridEnabled: !gridEnabled });
     };
 
+    const button = createMenuItem('Enable grid', onClick);
+
     // handle state updates
     addStateObserver(({ gridEnabled }) => {
-        const button = document.getElementById(id);
         if (button) {
             button.innerText = `${gridEnabled ? 'Disable' : 'Enable'} grid`;
         }
     });
-
-    const button = createMenuItem('Enable grid', onClick);
-    button.setAttribute('id', id);
 
     return button;
 }
