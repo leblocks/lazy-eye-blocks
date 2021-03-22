3f34b54fbc77556b2e403381b143b2c254ec3a24
sashag <a.f.gurevich@gmail.com>
2021-03-22 18:04:04 +0200

    added info to game over screen

--- src/ui/game-over-menu/index.js
+++ src/ui/game-over-menu/index.js
@@ -6,19 +6,23 @@ import {
 } from '../utils';
 
 
+const getLevelMenuTitle = (speedLevel) => `Level: ${speedLevel}`;
 const getScoreMenuTitle = (score) => `Score: ${score}`;
 
 export default function () {
-    const { score: currentScore } = getState();
+    const { score: currentScore, speedLevel: currentSpeedLevel } = getState();
     const scoreTitle = createMenuItemText(getScoreMenuTitle(currentScore));
+    const speedLevelTitle = createMenuItemText(getLevelMenuTitle(currentSpeedLevel));
 
-    addStateObserver(['score'], ({ score }) => {
+    addStateObserver(['score', 'speedLevel'], ({ score, speedLevel }) => {
         scoreTitle.innerHTML = getScoreMenuTitle(score);
+        speedLevelTitle.innerHTML = getLevelMenuTitle(speedLevel);
     });
 
     return createMenu([
         createMenuTitle('Game over'),
         scoreTitle,
+        speedLevelTitle,
         createMenuItem('Back', () => goto(MAIN_MENU_STATE)),
     ]);
 }
