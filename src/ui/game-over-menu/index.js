4f3d382ea978ea0825538ac579211d4b6c1ad187
sashag <a.f.gurevich@gmail.com>
2021-03-22 17:59:29 +0200

    game over screen

--- src/ui/game-over-menu/index.js
+++ src/ui/game-over-menu/index.js
@@ -1,8 +1,24 @@
-import { createMenu, createMenuTitle } from '../utils';
+import { addStateObserver, getState } from '../../state';
+import { MAIN_MENU_STATE } from '../../state/consts';
+import { goto } from '../../utils';
+import {
+    createMenu, createMenuItem, createMenuItemText, createMenuTitle,
+} from '../utils';
 
+
+const getScoreMenuTitle = (score) => `Score: ${score}`;
 
 export default function () {
+    const { score: currentScore } = getState();
+    const scoreTitle = createMenuItemText(getScoreMenuTitle(currentScore));
+
+    addStateObserver(['score'], ({ score }) => {
+        scoreTitle.innerHTML = getScoreMenuTitle(score);
+    });
+
     return createMenu([
         createMenuTitle('Game over'),
+        scoreTitle,
+        createMenuItem('Back', () => goto(MAIN_MENU_STATE)),
     ]);
 }
