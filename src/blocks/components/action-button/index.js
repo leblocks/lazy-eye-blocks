528811670d2bc210b0f52383c1554e297b6cca11
Alexander Gurevich <alexander.gurevich@earnix.com>
2021-03-30 15:29:34 +0300

    added unfocus functionality for better keyboard support

--- src/blocks/components/action-button/index.js
+++ src/blocks/components/action-button/index.js
@@ -1,9 +1,19 @@
 import { createElement, getClassList } from '../../../web-api-polyfills';
 
+const unfocusButtons = () => {
+    // we have to unfocus buttons after each click
+    // so those buttons won't be focused when keyboard is touched
+    document.querySelectorAll('button')
+        .forEach((button) => button.blur());
+};
+
 export default function (buttonTitle, onClickHandler) {
     const button = createElement('button');
     getClassList(button).add('action-button');
-    button.onclick = onClickHandler;
+    button.onclick = () => {
+        onClickHandler();
+        unfocusButtons();
+    };
     button.innerHTML = buttonTitle;
     return button;
 }
