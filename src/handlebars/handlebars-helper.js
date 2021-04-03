763871b511cb2f12775bc71770389bfde6063c88
sashag <a.f.gurevich@gmail.com>
2021-04-03 16:30:58 +0300

    improved page generation infra

--- src/handlebars/handlebars-helper.js
+++ src/handlebars/handlebars-helper.js
@@ -2,11 +2,16 @@
 // it will be executed by node
 // eslint-disable-next-line import/no-extraneous-dependencies
 const hb = require('handlebars');
-const { readFileSync } = require('fs');
+const path = require('path');
+const fs = require('fs');
 
-const getCompiledTemplate = (pathToTemplate) => {
-    const content = readFileSync(pathToTemplate, 'utf-8');
-    return hb.compile(content);
+const rootTemplateContent = fs.readFileSync(path.resolve(__dirname, 'root.hbs'), 'utf-8');
+const rootTemplate = hb.compile(rootTemplateContent);
+
+module.exports = {
+    getRootTemplate: () => rootTemplate,
+    getCompiledTemplate: (pathToTemplate) => {
+        const content = fs.readFileSync(path.resolve(__dirname, pathToTemplate), 'utf-8');
+        return hb.compile(content);
+    },
 };
-
-module.exports = { getCompiledTemplate };
